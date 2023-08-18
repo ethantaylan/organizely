5db41411ed4db4bb52654747b73254e4ecc9f685
Ethan TAYLAN <59540282+ethantaylan@users.noreply.github.com>
2023-08-18 02:39:33 +0200

    Update layout.tsx

--- src/pages/layout/layout.tsx
+++ src/pages/layout/layout.tsx
@@ -1,4 +1,4 @@
-import React, { PropsWithChildren } from "react
+import React, { PropsWithChildren } from "react"
 import { Header } from "./layout/header";
 
 export const AppLayout: React.FC<PropsWithChildren> = ({ children }) => {
